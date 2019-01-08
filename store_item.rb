77151aa01e06fc14cbebfe1edfb00e0f18451981
bryewalks <bryewalks@gmail.com>
2019-01-08 17:01:08 -0600

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,6 @@
+football = {:price => 10, :color => "brown", :weight => 1}
+basketball = {:price => 8, :color => "orange", :weight => 2} 
+soccor_ball = {price: 12, color: "white", weight: 3}
+
+puts soccor_ball[:price]
+puts basketball[:price]
