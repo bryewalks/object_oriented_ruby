c82d87114f174b2b16661f1c8bc1029c2aa31e76
bryewalks <bryewalks@gmail.com>
2019-01-09 17:14:47 -0600

    day 3 exercise

---
+++ employee.rb
@@ -0,0 +1,81 @@
+
+class Employee
+
+  attr_reader :first_name, :last_name, :active
+  attr_writer :active #line 43 shows a writer method written out.
+
+  def initialize(input_options)
+    
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
+
+  end
+
+
+  def print_info
+
+   puts "#{@first_name} #{@last_name} makes #{@salary} a year."
+
+  end
+
+
+  def give_annual_raise
+
+    @salary *= 1.05
+
+  end
+end
+
+class Manager < Employee
+
+  def initialize(input_options)
+    super(input_options)
+    @employees = input_options [:employees]
+  end
+
+  def send_report
+
+    puts "Sending report..."
+
+    #code
+
+    puts "Email sent."
+
+  end
+
+end
+
+
+employee_1 = Employee.new(
+                            first_name: "Nick",
+                            last_name: "Cage", 
+                            salary: 70000, 
+                            active: true
+                            )
+
+employee_2 = Employee.new(
+                            first_name: "Julia", 
+                            last_name: "Andrews", 
+                            salary: 80000, 
+                            active: true
+                            )
+
+
+employee_1.print_info
+employee_2.print_info
+
+manager = Manager.new(
+
+                        first_name: "Grace",
+                        last_name: "Hopper",
+                        salary: 100000,
+                        active: true,
+                        employees: [employee_1, employee_2]
+                        )
+
+p manager
+# manager.print_info
+# manager.send_report
+
--- store_item.rb
+++ store_item.rb
@@ -1,6 +1,63 @@
-football = {:price => 10, :color => "brown", :weight => 1}
-basketball = {:price => 8, :color => "orange", :weight => 2} 
-soccor_ball = {price: 12, color: "white", weight: 3}
+class Store_Item
 
-puts soccor_ball[:price]
-puts basketball[:price]
+attr_reader :price, :color, :weight
+attr_writer :price
+
+
+def initialize(name_input, price_input, color_input, weight_input)
+
+  @price = price_input
+  @color = color_input
+  @weight = weight_input
+
+end
+
+
+def print_info
+
+  p "#{@price} #{@color} #{@weight}"
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
+
+
+
+
+football = Store_Item.new("football", 10, "brown", 1)
+basketball = Store_Item.new(8, "orange", 2)
+football = Store_Item.new(12, "white", 3)
+
+football.print_info
+
+
+
+
+# basketball =  (8,  "orange", 2)
+# soccor_ball =  (12, "white", 3)
+
+# puts basketball.price
